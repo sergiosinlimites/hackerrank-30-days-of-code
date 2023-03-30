e21b96ed9216278f1e4bc2db8755c52cec61336d
Sergio Andrés Bolaños Penagos <sergioandresbolanospenagos@gmail.com>
2023-03-29 21:20:26 -0500

    Day 6 challenge completed

---
+++ src/Day6ReviewLoop/Day6ReviewLoop.java
@@ -0,0 +1,25 @@
+package Day6ReviewLoop;
+
+import java.util.Scanner;
+
+public class Day6ReviewLoop {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int iterations = Integer.parseInt(scanner.nextLine());
+
+    for(int i = 0; i < iterations; i++){
+      StringBuffer evenCharsBuffer = new StringBuffer();
+      StringBuffer oddCharsBuffer = new StringBuffer();
+      String text = scanner.nextLine();
+      for(int j = 0; j < text.length(); j++){
+        if(j % 2 == 0){
+          evenCharsBuffer.append(String.valueOf(text.charAt(j)));
+        } else {
+          oddCharsBuffer.append(String.valueOf(text.charAt(j)));
+        }
+      }
+      System.out.println(evenCharsBuffer.toString() + " " + oddCharsBuffer.toString());
+    }
+    scanner.close();
+  }
+}
