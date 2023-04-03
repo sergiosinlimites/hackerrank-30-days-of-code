a2d8d2a07f9525aca5413242693132439fef0537
Sergio Andrés Bolaños Penagos <sergioandresbolanospenagos@gmail.com>
2023-04-03 18:53:38 -0500

    Day 19 challenge completed

---
+++ src/Day19Interfaces/Day19Interfaces.java
@@ -0,0 +1,42 @@
+package Day19Interfaces;
+
+/**
+ * Here is the problem: https://www.hackerrank.com/challenges/30-interfaces/problem
+ */
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Day19Interfaces {
+  
+}
+
+interface AdvancedArithmetic{
+  int divisorSum(int n);
+}
+
+class Calculator implements AdvancedArithmetic {
+  public int divisorSum(int n) {
+    int result = 0;
+    for(int i = 1; i <= n; i++){
+      if(n % i == 0){
+        result += i;
+      }
+    }
+    return result;
+  }
+}
+
+class Solution {
+
+  public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
+      int n = scan.nextInt();
+      scan.close();
+      
+      AdvancedArithmetic myCalculator = new Calculator(); 
+      int sum = myCalculator.divisorSum(n);
+      System.out.println("I implemented: " + myCalculator.getClass().getInterfaces()[0].getName() );
+      System.out.println(sum);
+  }
+}
